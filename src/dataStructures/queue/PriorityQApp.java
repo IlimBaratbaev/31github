061b0c48e937c1c18b1a04477d2bfbf238234769
Ilim <ilimzero4vich@gmail.com>
2023-12-04 20:37:34 +0600

    day 4 learned priority queue

---
+++ src/dataStructures/queue/PriorityQApp.java
@@ -0,0 +1,16 @@
+package dataStructures.queue;
+
+public class PriorityQApp {
+    public static void main(String[] args) {
+        PriorityQ priorityQ = new PriorityQ(5);
+        priorityQ.insert(30);
+        priorityQ.insert(60);
+        priorityQ.insert(10);
+        priorityQ.insert(40);
+        priorityQ.insert(20);
+        while (!priorityQ.isEmpty()) {
+            System.out.print(priorityQ.remove() + " ");
+        }
+        System.out.println();
+    }
+}
