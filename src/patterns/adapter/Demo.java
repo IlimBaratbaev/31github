c7a9d774f55f0bf3805cd6a3e7b7d38fd7673d21
Ilim <ilimzero4vich@gmail.com>
2023-12-18 22:20:40 +0600

    day 18. started recursion part

--- src/patterns/adapter/Demo.java
+++ src/patterns/adapter/Demo.java
@@ -4,6 +4,18 @@ public class Demo {
     public static void main(String[] args) {
         RoundHole hole = new RoundHole(5);
         RoundPeg peg = new RoundPeg(5);
-        if (hole.fits())
+        if (hole.fits(peg)) {
+            System.out.println("Round peg fits to round hole");
+        }
+        SquarePeg smallSqPeg = new SquarePeg(2);
+        SquarePeg largeSqPeg = new SquarePeg(10);
+        SquarePegAdapter smallSquarePegAdapter = new SquarePegAdapter(smallSqPeg);
+        SquarePegAdapter largeSquarePegAdapter = new SquarePegAdapter(largeSqPeg);
+        if (hole.fits(smallSquarePegAdapter)) {
+            System.out.println("Square peg fits to round hole");
+        }
+        if (!hole.fits(largeSquarePegAdapter)) {
+            System.out.println(" large Square peg fits to round hole");
+        }
     }
 }
